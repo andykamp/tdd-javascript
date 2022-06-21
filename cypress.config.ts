6a0038be6e09bcf3dfba62fc39cc45e957f075eb
andykamp <akampenes@gmail.com>
2022-06-21 16:20:28 +0200

    initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "swxhgh",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ test/stack.test.ts
@@ -0,0 +1,51 @@
+class Stack {
+  top: number;
+  items: any;
+
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+
+  get peek() {
+    return this.items[this.top];
+  }
+
+  push(value: any) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+
+  pop() {
+    const popped = this.items[this.top];
+    this.top -= 1;
+    delete this.items[this.top];
+    return popped;
+  }
+}
+
+describe("My Stack", () => {
+  let stack: Stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("iscreatede empty", () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it("can push to the top", () => {
+    stack.push("item");
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("item");
+  });
+
+  it("can poop off", () => {
+    stack.push("item");
+    const popped = stack.pop();
+    expect(popped).toBe("item");
+    expect(stack.top).toEqual(-1);
+  });
+});
